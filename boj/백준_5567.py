877975dacd9bc2c21e5ae8cca4d91e0a11734926
June0505 <87131779+June0505@users.noreply.github.com>
2021-12-20 19:50:55 +0900

    boj

---
+++ boj/백준_5567.py
@@ -0,0 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+M = int(input())
+
+person = []
+friend = []
+friend_of_friend = []
+
+for i in range(M):
+  a, b = map(int, input().split())
+  if a == 1:
+    friend.append(b)
+  else:
+    tmp = [a, b]
+    person.append(tmp)
+
+for i in person:
+  for j in friend:
+    if j in i: 
+      friend_of_friend.append(i[0])
+      friend_of_friend.append(i[1])
+
+result = friend + friend_of_friend
+set_result = set(result)
+result = list(set_result)
+
+print(len(result))
+
+# https://www.acmicpc.net/problem/5567
