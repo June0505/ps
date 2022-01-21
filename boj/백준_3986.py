4bbf828d150c8c2abff2fa38b12914f463103fc9
June0505 <87131779+June0505@users.noreply.github.com>
2022-01-21 18:23:06 +0900

    3986

---
+++ boj/백준_3986.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+result = 0
+
+for _ in range(n):
+  s = input().strip()
+  stack = []
+  for i in s:
+    if not stack or stack[-1] != i:
+      stack.append(i)
+    else:
+      stack.pop()
+
+  if not stack:
+    result += 1
+
+print(result)
+
+# https://www.acmicpc.net/problem/3986
